f1a61b6b9d7d5faf4455e80d8f3df324fb670828
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-09-16 12:46:44 -0400

    Add Secondary Light Source

--- src/script.js
+++ src/script.js
@@ -40,13 +40,41 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
-// Red Ligh
-const pointLight2 = new THREE.PointLight(0xff0000, 2)
-pointLight2.position.set(1, 1, 1)
-pointLight2.intensity = 1
+// White Light
+const pointLight2 = new THREE.PointLight(0xFFFFFF, 2)
+pointLight2.position.set(0, -3.6, 0)
+pointLight2.intensity = 6
+
+// Create GUI Folder
+const light1 = gui.addFolder('Light 1')
+
+light1.add(pointLight2.position, 'x')
+light1.add(pointLight2.position, 'y')
+light1.add(pointLight2.position, 'z')
+light1.add(pointLight2, 'intensity')
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
+// scene.add(pointLightHelper)
+
+// Blue Light
+const pointLight3 = new THREE.PointLight(0x0000FF, 2)
+pointLight3.position.set(18, 9, 1)
+pointLight3.intensity = 10
+
+// Create GUI Folder
+const light2 = gui.addFolder('Light 2')
+
+light2.add(pointLight3.position, 'x')
+light2.add(pointLight3.position, 'y')
+light2.add(pointLight3.position, 'z')
+light2.add(pointLight3, 'intensity')
+
+// const pointLightHelperBlue = new THREE.PointLightHelper(pointLight3, 1)
+// scene.add(pointLightHelperBlue)
 
 scene.add(pointLight)
 scene.add(pointLight2)
+scene.add(pointLight3)
 
 /**
  * Sizes
